426ce935d7aa06dd4e73ebadbfcc60800bfe0fb1
JACE <jacexin77@gmail.com>
2025-05-22 14:49:40 +1200

    restart

--- frontend/src/app/pages/products/products.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-
-export const PRODUCTS_ROUTES: Routes = [
-  { path: '', component: ProductListComponent },
-  { path: ':id', component: ProductDetailComponent },
-  { path: '**', redirectTo: '' },
-];
